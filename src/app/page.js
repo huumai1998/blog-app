03bc27df0e82035fe9e04a323859c849dda3acca
huumai1998 <huumai0918@gmail.com>
2023-09-05 06:13:11 -0600

    addSomeStuff

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import { Featured, CategoryList, CardList, Menu } from "@/components";
-import Link from "next/link";
 
 export default function Home() {
   return (
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
 import React from "react";
+import { FiFacebook, FiGithub } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 export const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="social">
-        <Image src="./facebook.png" />
+        <div className="icons">
+          <FiFacebook className="icon face" />
+          <AiOutlineInstagram className="icon insta" />
+          <FiGithub className="icon github" />
+        </div>
       </div>
       <div className="logo"></div>
       <div className="link"></div>
