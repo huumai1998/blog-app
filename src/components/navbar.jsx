8a40477479c62377fe01dfc1cf8cbaaa8eeddb85
huumai1998 <huumai0918@gmail.com>
2023-09-05 04:47:19 -0600

    tmr

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 export const Navbar = () => {
   return (
     <div className="nav-container">
-      <div className="social"></div>
+      <div className="social">
+        <Image src="./facebook.png" />
+      </div>
       <div className="logo"></div>
       <div className="link"></div>
     </div>
