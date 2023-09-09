01a9fe775c72bdf80dc2795adf43e438a8ff9e30
huumai1998 <huumai0918@gmail.com>
2023-09-09 17:54:58 -0600

    navbar-2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,13 +7,13 @@ import { AuthLinks, ThemeToggle } from ".";
 export const Navbar = () => {
   return (
     <div className="nav-container">
-      <div className="social">
+      {/* <div className="social">
         <div className="icons">
           <FiFacebook className="icon face" />
           <AiOutlineInstagram className="icon insta" />
           <FiGithub className="icon github" />
         </div>
-      </div>
+      </div> */}
       <div className="logo">
         <Link href="/" className="">
           LOGO HERE
