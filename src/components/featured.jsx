72e578bd9fe8994f593cbd0324106a97bb33231d
huumai1998 <huumai0918@gmail.com>
2023-09-11 22:08:48 -0600

    startWorkingFeature-v1

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const Featured = () => {
-  return <div className="feature-container">featured</div>;
+  return (
+    <div className="feature-container">
+      <h1 className="feature-title">
+        <b>Hey, It is dev here!</b>
+      </h1>
+      <div className="feature-post">
+        <div className="feature-imgContainer"></div>
+        <div className="feature-textContainer"></div>
+      </div>
+    </div>
+  );
 };
