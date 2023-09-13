801239cb81415c1ab253a29542b2f7531a0e0cf6
huumai1998 <huumai0918@gmail.com>
2023-09-12 22:51:37 -0600

    idk what's this

--- src/components/categoryList.jsx
+++ src/components/categoryList.jsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export const CategoryList = () => {
-  return <div className="category-container">category</div>;
+  return (
+    <div className="categories-container">
+      <h1 className="categories-title">Popular Categories</h1>
+      <div className="categories">
+        <Link href="/blog?cat=style" className="catetory style">
+          <Image src="/" width={32} height={32} className="categories-image" />
+          Style
+        </Link>
+      </div>
+    </div>
+  );
 };
