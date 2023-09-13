f53d11da60e6c2952ca0d09836ffe02109dcc8dc
huumai1998 <huumai0918@gmail.com>
2023-09-12 23:06:47 -0600

    done catetgorue

--- src/components/categoryList.jsx
+++ src/components/categoryList.jsx
@@ -7,9 +7,65 @@ export const CategoryList = () => {
     <div className="categories-container">
       <h1 className="categories-title">Popular Categories</h1>
       <div className="categories">
-        <Link href="/blog?cat=style" className="catetory style">
-          <Image src="/" width={32} height={32} className="categories-image" />
+        <Link href="/blog?cat=style" className="category style">
+          <Image
+            src="/style.jpg"
+            alt="style"
+            width={32}
+            height={32}
+            className="categories-image"
+          />
           Style
+        </Link>
+        <Link href="/blog?cat=style" className="category fashion">
+          <Image
+            src="/style.jpg"
+            alt="style"
+            width={32}
+            height={32}
+            className="categories-image"
+          />
+          Fashion
+        </Link>
+        <Link href="/blog?cat=style" className="category food">
+          <Image
+            src="/style.jpg"
+            alt="style"
+            width={32}
+            height={32}
+            className="categories-image"
+          />
+          Food
+        </Link>
+        <Link href="/blog?cat=style" className="category travel">
+          <Image
+            src="/style.jpg"
+            alt="style"
+            width={32}
+            height={32}
+            className="categories-image"
+          />
+          Travel
+        </Link>
+        <Link href="/blog?cat=style" className="category culture">
+          <Image
+            src="/style.jpg"
+            alt="style"
+            width={32}
+            height={32}
+            className="categories-image"
+          />
+          Culture
+        </Link>
+        <Link href="/blog?cat=style" className="category coding">
+          <Image
+            src="/style.jpg"
+            alt="style"
+            width={32}
+            height={32}
+            className="categories-image"
+          />
+          Coding
         </Link>
       </div>
     </div>
