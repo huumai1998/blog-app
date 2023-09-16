cb6d2f1b0caf08d19eeae940df1e6e37ec790375
huumai1998 <huumai0918@gmail.com>
2023-09-16 14:44:38 -0600

    1commit

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export const Menu = () => {
-  return <div className="menu-container">Menu</div>;
+  return (
+    <div className="menu-container">
+      <h2 className="subtitle">{"What's hot"}</h2>
+      <h1 className="title">Most Popular</h1>
+      <div className="items">
+        <Link href="/" className="item">
+          <div className="imageContainer">
+            <Image src="/test1.jpg" alt="" fill className="image" />
+          </div>
+          <div className="textContainer">
+            <span className="category travel">Travel</span>
+            <h3>
+              asdf adfa asf asdf asdf asf awfas fas af qawf asd as asdf asdf
+              awfe asd{" "}
+            </h3>
+            <div className="detail">
+              <span className="username">Nick Young</span>
+              <span className="date">18.10.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
