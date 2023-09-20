525a641b772013694c82148570f3fff3595ea8e6
huumai1998 <huumai0918@gmail.com>
2023-09-19 22:15:53 -0600

    dsf

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,31 +6,26 @@ import { AuthLinks, ThemeToggle } from ".";
 
 export const Navbar = () => {
   return (
-    <div className="nav-container">
-      {/* <div className="social">
-        <div className="icons">
-          <FiFacebook className="icon face" />
-          <AiOutlineInstagram className="icon insta" />
-          <FiGithub className="icon github" />
+    <div className="nav">
+      <div className="nav-container">
+        <div className="logo logo-typography">
+          <Link href="/" className="">
+            LOGO HERE
+          </Link>
         </div>
-      </div> */}
-      <div className="logo logo-typography">
-        <Link href="/" className="">
-          LOGO HERE
-        </Link>
-      </div>
-      <div className="links">
-        <ThemeToggle />
-        <Link href="#" className="link">
-          HomePage
-        </Link>
-        <Link href="#" className="link">
-          Contact
-        </Link>
-        <Link href="#" className="link">
-          About
-        </Link>
-        <AuthLinks />
+        <div className="links">
+          <ThemeToggle />
+          <Link href="#" className="link">
+            HomePage
+          </Link>
+          <Link href="#" className="link">
+            Contact
+          </Link>
+          <Link href="#" className="link">
+            About
+          </Link>
+          <AuthLinks />
+        </div>
       </div>
     </div>
   );
