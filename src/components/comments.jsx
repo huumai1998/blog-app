a50dafb2099eb821836dce4c47e52a9133324ef5
huumai1998 <huumai0918@gmail.com>
2023-09-28 21:58:02 -0600

    chahieugi

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -15,6 +16,17 @@ export const Comments = () => {
         ) : (
           <Link href="/login">Login to write a comments</Link>
         )}
+        <div className="comments">
+          <div className="comment">
+            <div className="user">
+              <Image className="image" />
+              <div className="userInfo">
+                <span className="username">John</span>
+                <span className="date">11.11.1111</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
