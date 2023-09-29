e304e5097f6336478790d3b8562a6f7c78e43728
huumai1998 <huumai0918@gmail.com>
2023-09-28 21:54:44 -0600

    d

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 export const Comments = () => {
+  const status = "authenticated";
   return (
     <>
-      <div className="comment-container">comments</div>
+      <div className="comment-container">
+        <h1 className="title">Comments</h1>
+        {status === "authenticated" ? (
+          <div className="write">
+            <textarea placeholder="Write a comment ..." className="input" />
+            <button className="button">Send</button>
+          </div>
+        ) : (
+          <Link href="/login">Login to write a comments</Link>
+        )}
+      </div>
     </>
   );
 };
