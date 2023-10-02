98cb23487e5e5f41db91467cbd3d75ce6d157443
huumai1998 <huumai0918@gmail.com>
2023-10-01 22:56:26 -0600

    addStuff

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Login = () => {
-  return <div>page</div>;
+  return (
+    <div className="page-container">
+      <div className="wrapper">
+        <div className="socialButton">Sign in with Google</div>
+        <div className="socialButton">Sign in with Github</div>
+        <div className="socialButton">Sign in with Facebook</div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
