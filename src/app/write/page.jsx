24f28de90b94f2966e8f7d284b407bd32aa32b49
huumai1998 <huumai0918@gmail.com>
2023-10-07 17:57:14 -0600

    addButton

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { AiOutlinePlusCircle } from "react-icons/ai";
+import { AiOutlinePlusCircle, AiOutlineVideoCameraAdd } from "react-icons/ai";
+import { BiImageAdd, BiLinkExternal } from "react-icons/bi";
 
 const Write = () => {
   const [open, setOpen] = useState(false);
@@ -12,19 +13,19 @@ const Write = () => {
     <div className="page-container">
       <input type="text" placeholder="Title" />
       <div className="editor">
-        <button className="write-page-button" onClick={() => setOpen(!open)}>
+        <button className="button" onClick={() => setOpen(!open)}>
           <AiOutlinePlusCircle className="plus-image" />
         </button>
         {open && (
           <div className="add">
-            <button className="write-page-button">
-              <AiOutlinePlusCircle className="plus-image" />
+            <button className="button">
+              <BiImageAdd className="plus-image" />
             </button>{" "}
-            <button className="write-page-button">
-              <AiOutlinePlusCircle className="plus-image" />
+            <button className="button">
+              <BiLinkExternal className="plus-image" />
             </button>{" "}
-            <button className="write-page-button">
-              <AiOutlinePlusCircle className="plus-image" />
+            <button className="button">
+              <AiOutlineVideoCameraAdd className="plus-image" />
             </button>
           </div>
         )}
