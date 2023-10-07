24a6594e77aaac66753c259fff4a6327af37ecc2
huumai1998 <huumai0918@gmail.com>
2023-10-07 17:43:08 -0600

    addReactIcon

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Write = () => {
-  return <div className="write-container">page</div>;
+  return (
+    <div className="page-container">
+      <input type="text" placeholder="Title" />
+      <div className="editor">
+        <button className="write-page-button"></button>
+      </div>
+    </div>
+  );
 };
 
 export default Write;
