45546fd26c51a7356203fb5990bd22c811463922
huumai1998 <huumai0918@gmail.com>
2023-10-07 17:50:53 -0600

    setOpen

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 const Write = () => {
+  const [open, setOpen] = useState(false);
+
+  // useEffect(() => {
+  //   setOpen(!open);
+  // });
   return (
     <div className="page-container">
       <input type="text" placeholder="Title" />
       <div className="editor">
-        <button className="write-page-button"></button>
+        <button className="write-page-button" onClick={() => setOpen(!open)}>
+          <AiOutlinePlusCircle className="plus-image" />
+        </button>
+        {open && (
+          <div className="add">
+            <button className="write-page-button">
+              <AiOutlinePlusCircle className="plus-image" />
+            </button>{" "}
+            <button className="write-page-button">
+              <AiOutlinePlusCircle className="plus-image" />
+            </button>{" "}
+            <button className="write-page-button">
+              <AiOutlinePlusCircle className="plus-image" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
