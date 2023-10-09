c3d13af2729fe90d02e633cb3cc6dd9985c6cf56
huumai1998 <huumai0918@gmail.com>
2023-10-09 00:52:11 -0600

    addNow

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,40 +1,47 @@
 "use client";
 import React, { useState } from "react";
-import { AiOutlinePlusCircle } from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
 import { BiImageAdd, BiLinkExternal } from "react-icons/bi";
 import { LiaPhotoVideoSolid } from "react-icons/lia";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
 
 const Write = () => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
 
   // useEffect(() => {
   //   setOpen(!open);
   // });
   return (
-    <div className="page-container">
-      <input type="text" placeholder="Title" />
+    <div className="write-page-container">
+      <input type="text" placeholder="Title" className="input" />
       <div className="editor">
-        <button className="button plus-button" onClick={() => setOpen(!open)}>
-          <AiOutlinePlusCircle className="plus-image" />
+        <button className="plus-button" onClick={() => setOpen(!open)}>
+          <AiOutlinePlus className="plus-image" />
         </button>
         {open && (
           <div className="add">
-            <button className="button children">
+            <button className="children">
               <BiImageAdd className="plus-image" />
             </button>{" "}
-            <button className="button children">
+            <button className="children">
               <BiLinkExternal className="plus-image" />
             </button>{" "}
-            <button className="button children">
+            <button className="children">
               <LiaPhotoVideoSolid className="plus-image" />
             </button>
           </div>
         )}
-        <ReactQuill theme="snow" />
+        <ReactQuill
+          className="textArea"
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story ..."
+        />
       </div>
+      <button className="publish">Publish</button>
     </div>
   );
 };
