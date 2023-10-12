c2edc33719f6c54bca8fb92e640113c20e182330
huumai1998 <huumai0918@gmail.com>
2023-10-11 21:07:29 -0600

    test

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const BlogPage = () => {
   return (
     <div className="page-container">
-      <h1 className="title-title">Style Blog</h1>
+      <h1 className="title-title">Style Blog!</h1>
       <div className="page-content">
         <CardList />
         <Menu />
