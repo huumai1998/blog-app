da0a9d5479d17970f2dd1e068cfb22f73eb4073b
huumai1998 <huumai0918@gmail.com>
2023-10-13 19:10:18 -0600

    addAPIPOST

--- src/components/cardList.jsx
+++ src/components/cardList.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import { Card, Pagination } from ".";
 import Image from "next/image";
 
-export const CardList = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed!");
+  }
+
+  return res.json();
+};
+
+export const CardList = async () => {
+  const data = await getData();
   return (
     <div className="cardList-container">
       <h1 className="title">Recent Posts</h1>
