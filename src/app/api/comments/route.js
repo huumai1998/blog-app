9f826d1e2dc5047863b2038e794e9bad92768673
huumai1998 <huumai0918@gmail.com>
2023-10-30 19:27:19 -0600

    updateAuth

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -23,3 +23,26 @@ export const GET = async (req) => {
     );
   }
 };
+
+// create a comments
+export const POST = async (req) => {
+  const { searchParams } = new URL(req.url);
+
+  const postSlug = searchParams.get("postSlug");
+
+  try {
+    const comments = await prisma.comment.findMany({
+      where: {
+        ...(postSlug && { postSlug }),
+      },
+      include: { user: true },
+    });
+
+    return new NextResponse(JSON.stringify(comments, { status: 200 }));
+  } catch (err) {
+    // console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
