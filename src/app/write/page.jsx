09637a296ec95084a3e733074c3ea2dff8310bfa
huumai1998 <huumai0918@gmail.com>
2023-11-11 17:00:04 -0700

    uoload

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BiImageAdd, BiLinkExternal } from "react-icons/bi";
 import { LiaPhotoVideoSolid } from "react-icons/lia";
@@ -7,14 +7,67 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "@/firebase";
 
+const storage = getStorage(app);
+// Write Page
 const Write = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
   const { data, status } = useSession();
   const [file, setFile] = useState(null);
+  const [media, setMedia] = useState("");
   const [title, setTitle] = useState("");
   const router = useRouter();
+
+  useEffect(() => {
+    const upload = () => {
+      const name = new Date().getTime + file.name;
+      const storageRef = ref(storage, name);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+          });
+        }
+      );
+    };
+    file && upload;
+  }, [file]);
 
   if (status === "loading") {
     return <div className="loading">Loading ...</div>;
@@ -24,9 +77,6 @@ const Write = () => {
     router.push("/");
   }
 
-  // useEffect(() => {
-  //   setOpen(!open);
-  // });
   return (
     <div className="write-page-container">
       <input type="text" placeholder="Title" className="input" />
